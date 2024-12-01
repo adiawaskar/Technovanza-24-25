0d55c5fc270f40a819f03bbfe9f0f73f56f1a424
Ritika Nankar <112073087+Ritika128@users.noreply.github.com>
2024-12-01 22:35:37 +0530

    added extras

--- frontend/src/components/PersonCard/PersonCard.jsx
+++ frontend/src/components/PersonCard/PersonCard.jsx
@@ -1,35 +1,37 @@
 import React from "react";
-import { FaInstagram, FaLinkedin, FaEnvelope } from "react-icons/fa"; // Import specific icons
+import { FaInstagram, FaLinkedin, FaEnvelope } from "react-icons/fa"; 
 import "./PersonCard.css";
 
-const PersonCard = ({ image, instagram, linkedin, email }) => {
+const PersonCard = ({ image, instagram, linkedin, email, showOverlay }) => {
   return (
-    <div className="person-card">
+    <div className={`person-card ${showOverlay ? 'with-overlay' : ''}`}>
       <img src={image} alt="person" className="person-image" />
-      <div className="overlay">
-        <a 
-          href={instagram} 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          className="social-icon"
-        >
-          <FaInstagram />
-        </a>
-        <a 
-          href={linkedin} 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          className="social-icon"
-        >
-          <FaLinkedin />
-        </a>
-        <a 
-          href={`mailto:${email}`} 
-          className="social-icon"
-        >
-          <FaEnvelope />
-        </a>
-      </div>
+      {showOverlay && (
+        <div className="overlay">
+          <a 
+            href={instagram} 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="social-icon"
+          >
+            <FaInstagram />
+          </a>
+          <a 
+            href={linkedin} 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="social-icon"
+          >
+            <FaLinkedin />
+          </a>
+          <a 
+            href={`mailto:${email}`} 
+            className="social-icon"
+          >
+            <FaEnvelope />
+          </a>
+        </div>
+      )}
     </div>
   );
 };
--- frontend/src/pages/Teams/Teams.jsx
+++ frontend/src/pages/Teams/Teams.jsx
@@ -15,21 +15,24 @@ const Teams = () => {
             <PersonCard image={require("../../components/Assets/Teams/pavanpatil.jpg")}
             instagram="https://www.instagram.com/paavan_patil"
             linkedin="https://www.linkedin.com/in/paavan-patil-01496323b"
-            email="patilpaavan7286@gmail.com" />
+            email="patilpaavan7286@gmail.com"
+            showOverlay={true} />
           <p className="person-name">Paavan Patil</p>
         </div>
         <div className="card-with-name">
             <PersonCard image={require("../../components/Assets/Teams/anamkhanzada.jpg")}
              instagram="https://www.instagram.com/anam_khanzada_"
             linkedin="https://www.linkedin.com/in/anam-khanzada-095429305"
-            email="anam.khanzada26@gmail.com" />
+            email="anam.khanzada26@gmail.com"
+            showOverlay={true} />
             <p className="person-name">Anam Khanzada</p>
         </div>
         <div className="card-with-name">
             <PersonCard image={require("../../components/Assets/Teams/nayaneshgudla.jpg")} 
             instagram="https://www.instagram.com/g_nayanesh"
             linkedin="https://www.linkedin.com/in/nayanesh-gudla"
-            email="nayaneshgudla1408@gmail.com" />
+            email="nayaneshgudla1408@gmail.com"
+            showOverlay={true} />
           <p className="person-name">Nayanesh Gudla</p>
         </div>
       </div>
@@ -41,57 +44,222 @@ const Teams = () => {
             <PersonCard image={require("../../components/Assets/Teams/ritikanankar.jpg")}
             instagram="https://www.instagram.com/ritikanankar____"
             linkedin="https://www.linkedin.com/in/ritikanankar"
-            email="nankarritika12@gmail.com" />
+            email="nankarritika12@gmail.com"
+            showOverlay={true} />
+            
             <p className="person-name">Ritika Nankar</p>
         </div>
         <div className="card-with-name">
           <PersonCard image={require("../../components/Assets/Teams/adiawaskar.jpg")}
            instagram="https://www.instagram.com/heyyitsadiiii"
             linkedin="https://www.linkedin.com/in/adiawaskar"
-            email="adiawaskar04@gmail.com" />
+            email="adiawaskar04@gmail.com"
+            showOverlay={true} />
           <p className="person-name">Adi Awaskar</p>
         </div>
       </div>
     </div>,
-    <div className="team-container" key="team-container-1">
-      <TeamCard image={require("../../components/Assets/Teams/treasurers.png")} teamName="Team Alpha" />
-    </div>,
-    <div className="team-container" key="team-container-2">
-      <TeamCard image={require("../../components/Assets/Teams/spons.png")} teamName="Team Beta" />
+    <div className="person-container" key="person-container">
+    <p className="general-heading">Treasurer</p>
+    <div className="person-cards">
+      <div className="card-with-name">
+          <PersonCard image={require("../../components/Assets/Teams/nirmalpatil.jpg")}
+          showOverlay={false} />
+          <p className="person-name">Nirmal Patil</p>
+      </div>
+      <div className="card-with-name">
+        <PersonCard image={require("../../components/Assets/Teams/shashinvathode.jpg")}
+         showOverlay={false} />
+        <p className="person-name">Shashin Vathode</p>
+      </div>
+    </div>
+  </div>,
+    <div className="person-container" key="person-container">
+    <p className="general-heading">Sponsorship</p>
+    <div className="person-cards">
+      <div className="card-with-name">
+          <PersonCard image={require("../../components/Assets/Teams/kabirshah.jpg")}
+           showOverlay={false}/>
+          <p className="person-name">Kabir Shah</p>
+      </div>
+      <div className="card-with-name">
+        <PersonCard image={require("../../components/Assets/Teams/dineshpatel.jpg")}
+         showOverlay={false} />
+        <p className="person-name">Dinesh Patel</p>
+      </div>
+    </div>
+  </div>,
+  <div className="team-container" key="team-container-1">
+      <TeamCard image={require("../../components/Assets/Teams/sponsorship.png.png")} teamName="Team Alpha" />
     </div>,
     // <div className="team-container" key="team-container-3">
     //   <p className="team-name">Web Development</p>
     //   <TeamCard image={require("../../components/Assets/Teams/webdev.png")} teamName="Team Gamma" />
     // </div>,
-    <div className="team-container" key="team-container-4">
-      <TeamCard image={require("../../components/Assets/Teams/pr.png")} teamName="Team Delta" />
+    <div className="person-container" key="person-container">
+    <p className="general-heading">Public Relations</p>
+    <div className="person-cards">
+      <div className="card-with-name">
+          <PersonCard image={require("../../components/Assets/Teams/aditinasare.jpg")}
+          showOverlay={false} />
+          <p className="person-name">Aditi Nasare</p>
+      </div>
+      <div className="card-with-name">
+        <PersonCard image={require("../../components/Assets/Teams/ishayupotey.jpg")}
+         showOverlay={false} />
+        <p className="person-name">Ishayu Potey</p>
+      </div>
+    </div>
+  </div>,
+  <div className="team-container" key="team-container-1">
+      <TeamCard image={require("../../components/Assets/Teams/pradmin.png.png")} teamName="Team Alpha" />
     </div>,
-    <div className="team-container" key="team-container-5">
-      <TeamCard image={require("../../components/Assets/Teams/ipos.png")} teamName="Team Epsilon" />
+    <div className="person-container" key="person-container">
+    <p className="general-heading">Internal Publicity Officers</p>
+    <div className="person-cards">
+      <div className="card-with-name">
+          <PersonCard image={require("../../components/Assets/Teams/aayushkhirraiyya.jpg")}
+          showOverlay={false} />
+          <p className="person-name">Aayush Khirraiyya</p>
+      </div>
+      <div className="card-with-name">
+        <PersonCard image={require("../../components/Assets/Teams/ishabarhate.jpg")}
+        showOverlay={false} />
+        <p className="person-name">Isha Barhate</p>
+      </div>
+      <div className="card-with-name">
+            <PersonCard image={require("../../components/Assets/Teams/jayashbhuyar.jpg")}
+             showOverlay={false}/>
+            <p className="person-name">Jayash Bhuyar</p>
+        </div>
+    </div>
+  </div>,
+    <div className="person-container" key="person-container">
+    <p className="general-heading">Marketing</p>
+    <div className="person-cards">
+      <div className="card-with-name">
+          <PersonCard image={require("../../components/Assets/Teams/khadijakb.jpg")}
+          showOverlay={false} />
+          <p className="person-name">Khadija KB</p>
+      </div>
+      <div className="card-with-name">
+        <PersonCard image={require("../../components/Assets/Teams/smitsatra.jpg")}
+         showOverlay={false} />
+        <p className="person-name">Smit Satra</p>
+      </div>
+    </div>
+  </div>,
+  <div className="team-container" key="team-container-1">
+      <TeamCard image={require("../../components/Assets/Teams/marketing.png.png")} teamName="Team Alpha" />
     </div>,
-    <div className="team-container" key="team-container-6">
-      <TeamCard image={require("../../components/Assets/Teams/marketing.png")} teamName="Team Epsilon" />
+  <div className="person-container" key="person-container">
+  <p className="general-heading">Operations-Bots</p>
+  <div className="person-cards">
+    <div className="card-with-name">
+        <PersonCard image={require("../../components/Assets/Teams/amrutgotpagar.jpg")}
+        showOverlay={false} />
+        <p className="person-name">Amrut Gotpagar</p>
+    </div>
+    <div className="card-with-name">
+      <PersonCard image={require("../../components/Assets/Teams/shwetakare.jpg")}
+       showOverlay={false} />
+      <p className="person-name">Shweta Kare</p>
+    </div>
+  </div>
+</div>,
+<div className="team-container" key="team-container-1">
+      <TeamCard image={require("../../components/Assets/Teams/operationsbots.png.png")} teamName="Team Alpha" />
     </div>,
-    <div className="team-container" key="team-container-7">
-      <TeamCard image={require("../../components/Assets/Teams/opsbots1.png")} teamName="Team Epsilon" />
+    
+    <div className="person-container" key="person-container">
+    <p className="general-heading">Operations-Coding</p>
+    <div className="person-cards">
+      <div className="card-with-name">
+          <PersonCard image={require("../../components/Assets/Teams/harshalamahajan.jpg")}
+          showOverlay={false}/>
+          <p className="person-name">Harshala Mahajan</p>
+      </div>
+      <div className="card-with-name">
+        <PersonCard image={require("../../components/Assets/Teams/nishitkekane.jpg")}
+         showOverlay={false} />
+        <p className="person-name">Nishit Kekane</p>
+      </div>
+    </div>
+  </div>,
+  <div className="team-container" key="team-container-1">
+      <TeamCard image={require("../../components/Assets/Teams/operationscoding.png.png")} teamName="Team Alpha" />
     </div>,
-    <div className="team-container" key="team-container-8">
-      <TeamCard image={require("../../components/Assets/Teams/opsbots2.png")} teamName="Team Epsilon" />
+    <div className="person-container" key="person-container">
+    <p className="general-heading">Design</p>
+    <div className="person-cards">
+      <div className="card-with-name">
+          <PersonCard image={require("../../components/Assets/Teams/yashphuke.jpg")}
+          showOverlay={false} />
+          <p className="person-name">Yash Phuke</p>
+      </div>
+      <div className="card-with-name">
+        <PersonCard image={require("../../components/Assets/Teams/eeshanajarekar.jpg")}
+         showOverlay={false} />
+        <p className="person-name">Eeshan Ajarekar</p>
+      </div>
+    </div>
+  </div>,
+  <div className="team-container" key="team-container-1">
+      <TeamCard image={require("../../components/Assets/Teams/designs.png.png")} teamName="Team Alpha" />
     </div>,
-    <div className="team-container" key="team-container-5">
-      <TeamCard image={require("../../components/Assets/Teams/opscoding.png")} teamName="Team Epsilon" />
+    <div className="person-container" key="person-container">
+    <p className="general-heading">Execution</p>
+    <div className="person-cards">
+      <div className="card-with-name">
+          <PersonCard image={require("../../components/Assets/Teams/yuktinbakshi.jpg")}
+          showOverlay={false} />
+          <p className="person-name">Yuktin Bakshi</p>
+      </div>
+      <div className="card-with-name">
+        <PersonCard image={require("../../components/Assets/Teams/shubhamkamble.jpg")}
+         showOverlay={false} />
+        <p className="person-name">Shubham Kamble</p>
+      </div>
+    </div>
+  </div>,
+  <div className="team-container" key="team-container-1">
+      <TeamCard image={require("../../components/Assets/Teams/execution.png.png")} teamName="Team Alpha" />
     </div>,
-    <div className="team-container" key="team-container-5">
-      <TeamCard image={require("../../components/Assets/Teams/design.png")} teamName="Team Epsilon" />
+    <div className="person-container" key="person-container">
+    <p className="general-heading">FINE ARTS & SOCIAL MEDIA</p>
+    <div className="person-cards">
+      <div className="card-with-name">
+          <PersonCard image={require("../../components/Assets/Teams/udaykumbhare.jpg")}
+          showOverlay={false} />
+          <p className="person-name">Uday Kumbhare</p>
+      </div>
+      <div className="card-with-name">
+        <PersonCard image={require("../../components/Assets/Teams/rasikamane.jpg")}
+        showOverlay={false} />
+        <p className="person-name">Rasika Mane</p>
+      </div>
+    </div>
+  </div>,
+  <div className="team-container" key="team-container-1">
+      <TeamCard image={require("../../components/Assets/Teams/fineartsa.png.png")} teamName="Team Alpha" />
     </div>,
-    <div className="team-container" key="team-container-5">
-      <TeamCard image={require("../../components/Assets/Teams/execution.png")} teamName="Team Epsilon" />
-    </div>,
-    <div className="team-container" key="team-container-5">
-      <TeamCard image={require("../../components/Assets/Teams/finearts.png")} teamName="Team Epsilon" />
-    </div>,
-    <div className="team-container" key="team-container-5">
-      <TeamCard image={require("../../components/Assets/Teams/vmco.png")} teamName="Team Epsilon" />
+    <div className="person-container" key="person-container">
+    <p className="general-heading">VJTI MUMBAI CUBE OPEN</p>
+    <div className="person-cards">
+      <div className="card-with-name">
+          <PersonCard image={require("../../components/Assets/Teams/achalnaikwadi.jpg")}
+          showOverlay={false} />
+          <p className="person-name">Achal Naikwadi</p>
+      </div>
+      <div className="card-with-name">
+        <PersonCard image={require("../../components/Assets/Teams/chitranshujha.jpg")}
+         showOverlay={false} />
+        <p className="person-name">Chitranshu Jha</p>
+      </div>
+    </div>
+  </div>,
+  <div className="team-container" key="team-container-1">
+      <TeamCard image={require("../../components/Assets/Teams/vmco.png.png")} teamName="Team Alpha" />
     </div>,
   ];
 
@@ -121,4 +289,3 @@ const Teams = () => {
 };
 
 export default Teams;
-
